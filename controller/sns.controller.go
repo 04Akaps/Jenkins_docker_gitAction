a33d1db90aa2097cd0a3d8ced05b5cb3dbee60e4
유호진 <hojin.yu@supercat.co.kr>
2023-04-27 14:06:30 +0900

    (feat) layout

---
+++ controller/sns.controller.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type SnsController struct {
+	Ctx context.Context
+}
+
+type SnsImpl interface {
+	GetSnsByID(http.ResponseWriter, *http.Request)
+	GetAllSnsByUserName(http.ResponseWriter, *http.Request)
+	MakeSns(http.ResponseWriter, *http.Request)
+}
+
+func NewSnsController() SnsImpl {
+	return &SnsController{Ctx: context.Background()}
+}
+
+func (sc *SnsController) GetSnsByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	log.Println("GetSnsByID", id)
+}
+
+func (sc *SnsController) GetAllSnsByUserName(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["userName"]
+
+	log.Println("GetAllSnsByUserName", name)
+}
+
+func (sc *SnsController) MakeSns(w http.ResponseWriter, r *http.Request) {
+	log.Println("MakeSns")
+}
---
+++ router/sns.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/04Akaps/Jenkins_docker_go.git/controller"
+)
+
+func (r *Router) SnsRouter() {
+	snsController := controller.NewSnsController()
+	snsPrefixUrl := r.router.PathPrefix("/sns").Subrouter()
+
+	snsPrefixUrl.HandleFunc("/getAll/{userName}", snsController.GetAllSnsByUserName).Methods("GET")
+	snsPrefixUrl.HandleFunc("/{id}", snsController.GetSnsByID).Methods("GET")
+	snsPrefixUrl.HandleFunc("/makeSns", snsController.MakeSns).Methods("POST")
+}
